ca8a443f57fb41970090e758551ef06893d02ff4
WitherZuo <openworld189@gmail.com>
2019-03-30 08:59:11 +0800

    Change the structure of the blog folder and make a clean

--- publish.sh
+++ publish.sh
@@ -2,8 +2,6 @@ echo Cleaning public repo cache.
 hexo clean
 echo Generating md to HTML files.
 hexo g
-echo Compressing files...Waiting for a moment!
-python test.py>nul
 echo Publishing all files to repo.
 hexo d
 echo Published all files!
@@ -11,3 +9,5 @@ echo Submitting your urls to Baidu...
 cd public/
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.wzblog.fun&token=TwFhN5gM4BMIe0a9" 
 cd /g/HexoBlog
+echo *****WARNING!*****
+echo This blog also have GitLab repo, PLEASE REMENBER PUSH ALL COMMITS TO GITLAB!
