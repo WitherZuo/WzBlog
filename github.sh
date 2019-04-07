11bd046d14ac17ff64c34c174cafd74ee88858f5
WitherZuo <openworld189@gmail.com>
2019-04-07 20:31:18 +0800

    Add reading progress bar and loading animation. Change gitlab ci file to add search and github. Add new 404 page. Tweak folders.
    Signed-off-by: WitherZuo <openworld189@gmail.com>

--- github.sh
+++ github.sh
@@ -1,3 +1,7 @@
+echo Publishing blog to GitHub Pages.
+echo *****WARNING!*****
+echo This blog also have GitLab repo, PLEASE REMENBER PUSH ALL COMMITS TO GITLAB!
+echo ======BEGIN!======
 echo Cleaning public repo cache.
 hexo clean
 echo Generating md to HTML files.
@@ -9,5 +13,6 @@ echo Submitting your urls to Baidu...
 cd public/
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.wzblog.fun&token=TwFhN5gM4BMIe0a9" 
 cd /g/HexoBlog
+echo ======FINISHED!======
 echo *****WARNING!*****
 echo This blog also have GitLab repo, PLEASE REMENBER PUSH ALL COMMITS TO GITLAB!
