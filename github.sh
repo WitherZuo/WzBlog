bbd4ce51194b6394804e84eb222fe69de44cca26
WitherZuo <openworld189@gmail.com>
2019-05-01 21:04:24 +0800

    remove curl and instead of baidu_url_submitter

--- github.sh
+++ github.sh
@@ -9,10 +9,6 @@ hexo g
 echo Publishing all files to repo.
 hexo d
 echo Published all files!
-echo Submitting your urls to Baidu...
-cd public/
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.wzblog.fun&token=TwFhN5gM4BMIe0a9" 
-cd /g/HexoBlog
 echo ======FINISHED!======
 echo *****WARNING!*****
 echo This blog also have GitLab repo, PLEASE REMENBER TO PUSH ALL COMMITS TO GITLAB!
