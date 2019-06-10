fa11852984a8d40c7fbfdf40806bd9dd5f2af12d
WitherZuo <openworld189@gmail.com>
2019-06-10 14:48:34 +0800

    test some variables.

--- runner.sh
+++ runner.sh
@@ -1,10 +1,13 @@
 echo Publishing blog to GitLab Pages.
+read -p "PLEASE ENTER COMMIT DESCRIPTION TEXT:" content
+echo -e " \n"
+echo "YOUR  COMMIT DESCRIPTION IS: $content"
 echo Adding all files modified to prepare commits.
 git add -A
 echo Checking status of all files before committing.
 git status
 echo Commit all files.
-git commit -m "commit to blog, updated."
+git commit -m "$content"
 echo Push all commits to GitLab repo.
 git push origin master
 echo Published all files to GitLab!
