699dca47ac4fde20ba3abebf7a7373b6f0ad2e9b
WitherZuo <openworld189@gmail.com>
2019-07-04 17:29:29 +0800

    add curl command-line to runner.

--- runner.sh
+++ runner.sh
@@ -13,3 +13,13 @@ git push origin master
 echo Published all files to GitLab!
 echo ======FINISHED!======
 echo GitLab was Done!
+echo -e "\n"
+echo Submitting all urls to Baidu.
+echo Generating url file with hexo g.
+hexo g
+echo Navigate to /public and find urls.txt.
+cd /g/HexoBlog/public
+echo Submit these urls to Baidu with Curl-cli.
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.wzblog.fun&token=TwFhN5gM4BMIe0a9"
+echo Submitted all urls to Baidu!
+echo ======FINISHED!======
