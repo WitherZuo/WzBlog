caf3cc98c926a4d911074d2bdadd6abbeac130ac
WitherZuo <openworld189@gmail.com>
2019-10-05 17:46:50 +0800

    fix hexo-resize-image script

--- themes/hexo-theme-next-7.4.1/source/js/hexo-resize-image.js
+++ themes/hexo-theme-next-7.4.1/source/js/hexo-resize-image.js
@@ -1,35 +1,27 @@
-function set_image_size(image, width, height) 
-{
+function set_image_size(image, width, height) {
     image.setAttribute("width", width + "px");
     image.setAttribute("height", height + "px");
 }
 
-function hexo_resize_image()
-{
+function hexo_resize_image() {
     var imgs = document.getElementsByTagName('img');
-    for (var i = imgs.length - 1; i >= 0; i--) 
-    {
+    for (var i = imgs.length - 1; i >= 0; i--) {
         var img = imgs[i];
         var src = img.getAttribute('src').toString();
         var fields = src.match(/\?(\d*x\d*)/);
-        if (fields && fields.length > 1)
-        {
+        if (fields && fields.length > 1) {
             var values = fields[1].split("x");
-            if (values.length == 2)
-            {
+            if (values.length == 2) {
                 var width = values[0];
                 var height = values[1];
 
-                if (!(width.length && height.length))
-                {
+                if (!(width.length && height.length)) {
                     var n_width = img.naturalWidth;
                     var n_height = img.naturalHeight;
-                    if (width.length > 0)
-                    {
+                    if (width.length > 0) {
                         height = n_height*width/n_width;
                     }
-                    if (height.length > 0)
-                    {
+                    if (height.length > 0) {
                         width = n_width*height/n_height;
                     }
                 }
@@ -39,8 +31,7 @@ function hexo_resize_image()
         }
 
         fields = src.match(/\?(\d*)/);
-        if (fields && fields.length > 1)
-        {
+        if (fields && fields.length > 1) {
             var scale = parseFloat(fields[1].toString());
             var width = scale/100.0*img.naturalWidth;
             var height = scale/100.0*img.naturalHeight;
