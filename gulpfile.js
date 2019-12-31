77049dff922b932b70dc3fa826eac15bbe409dab
WitherZuo <openworld189@gmail.com>
2019-12-31 23:35:19 +0800

    try to fix whitespace missing.

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,9 @@ gulp.task('minify-css', function() {
     return gulp.src('./public/**/*.css')
         .pipe(minifycss({
             advanced: true,//类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
-            compatibility: 'ie7',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+            compatibility: '*',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
             keepBreaks: true,//类型：Boolean 默认：false [是否保留换行]
-            keepSpecialComments: '*'
-            //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+            keepSpecialComments: '*'//保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
         }))
         .pipe(gulp.dest('./public'));
 });
@@ -28,6 +27,7 @@ gulp.task('minify-html', function() {
         .pipe(htmlmin({
             removeComments: true,//清除 HTML 注释
             collapseWhitespace: true,//压缩 HTML
+            preserveLineBreaks: true,//试图修正被错误取掉的换行符问题
             collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
             removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
             removeScriptTypeAttributes: true,//删除 <script> 的 type="text/javascript"
