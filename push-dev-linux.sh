72f18c110408d2a6e851014fc181ca2104cde23b
WitherZuo <openworld189@gmail.com>
2021-01-09 21:01:10 +0800

    run-script-os: 'npm run publish' => 'npm run push'

--- push-dev-linux.sh
+++ push-dev-linux.sh
@@ -1,7 +1,7 @@
 # 显示提示信息、切换分支为 develop
 echo This script will push all commits to remote develop branch,
 echo if you want to publish your blog for everyone, please use 
-echo 'npm run publish' command.
+echo 'npm run push' command.
 echo -e "\n"
 git branch
 git checkout develop
--- push-dev-windows.bat
+++ push-dev-windows.bat
@@ -3,7 +3,7 @@ title push-dev
 :: 显示提示信息、切换分支为 develop
 echo This script will push all commits to remote develop branch,
 echo if you want to publish your blog for everyone, please use 
-echo 'npm run publish' command.
+echo 'npm run push' command.
 echo.
 git branch 
 git checkout develop
