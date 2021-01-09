07544f5b0ea9baded28f1620b4ff25f8d3ea5a3a
WitherZuo <openworld189@gmail.com>
2021-01-09 20:14:40 +0800

    test push-dev-linux.sh

--- push-dev-windows.bat
+++ push-dev-windows.bat
@@ -1,6 +1,6 @@
 @echo off
 title push-dev
-
+:: 显示提示信息、切换分支为 develop
 echo This script will push all commits to remote develop branch,
 echo if you want to publish your blog for everyone, please use 
 echo 'npm run publish' command.
@@ -8,9 +8,11 @@ echo.
 git branch 
 git checkout develop
 git branch
+:: 输入 commit 的描述内容文本
 echo.
-set /p content=PLEASE ENTER COMMIT DESCRIPTION TEXT:
+set /p content=PLEASE ENTER COMMIT DESCRIPTION TEXT: 
 cls
+:: 显示 commit 的描述内容文本、提交并推送更改至远程分支
 echo YOUR COMMIT DESCRIPTION IS: %content%
 git status
 git add -A
--- runner.sh
+++ runner.sh
@@ -1,26 +1,29 @@
+# 切换本地分支为 master 分支，并将 develop 分支合并至 master 分支
 echo Publishing blog source-code to GitHub repo.
 echo Switching branch to master and merging from develop...
 git checkout master
 git branch
 git merge develop
-
+# 输入 commit 的描述内容文本
+echo -e "\n"
 read -p "PLEASE ENTER COMMIT DESCRIPTION TEXT:" content
-echo -e " \n"
+clear
+# 显示 commit 的描述内容文本、提交并推送更改至远程分支
 echo "YOUR COMMIT DESCRIPTION IS: $content"
-
+echo -e "\n"
 echo Adding all files modified to prepare commits.
 git add -A
 echo Checking status of all files before committing.
 git status
-echo Commit all files.
+echo Committing all files.
 git commit -m "$content"
 echo Push all commits to GitHub repo.
 git push origin master --force
 echo Published all files to GitHub!
 echo ======FINISHED!======
 echo GitHub was Done!
+# 提交网址到百度站长平台
 echo -e "\n"
-
 echo Submitting all urls to Baidu.
 echo Generating url file with hexo g.
 hexo g
@@ -31,6 +34,6 @@ echo Submit urls to Baidu with Curl-cli.
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://wzblog.fun&token=TwFhN5gM4BMIe0a9"
 echo Submitted all urls to Baidu!
 echo ======FINISHED!======
-
+# 将本地分支由 master 分支切换为 develop 分支，结束
 git checkout develop
 read -p "Press any key to continue." var
