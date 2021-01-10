4467a161dff5986ec8cd7f96c793f667029a37e5
WitherZuo <openworld189@gmail.com>
2021-01-10 23:46:02 +0800

    run-script-os: test address

---
+++ push/push-dev-windows.bat
@@ -0,0 +1,24 @@
+@echo off
+title push-dev
+:: 显示提示信息、切换分支为 develop
+echo This script will push all commits to remote develop branch,
+echo --if you want to publish your blog to everyone, please use 
+echo 'npm run push' command.--
+echo.
+git branch 
+git checkout develop
+git branch
+:: 输入 commit 的描述内容文本
+echo.
+set /p content=PLEASE ENTER COMMIT DESCRIPTION TEXT: 
+cls
+:: 显示 commit 的描述内容文本、提交并推送更改至远程分支
+echo YOUR COMMIT DESCRIPTION IS: %content%
+git status
+git add -A
+git status
+git commit -m "%content%"
+git push
+echo All commits were pushed to remote develop branch.
+pause
+exit
